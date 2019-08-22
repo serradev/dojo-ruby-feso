85e18d20cfbb7268aaef728ee2e0ca37c49f4cb3
Diego Sarmento Mendes <diego.s.mendes2310@gmail.com>
2019-08-22 18:16:15 -0300

    Geração de scaffold de entidade Article
    Comandos executados:
    $ rails generate scaffold Article title:string content:text
    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_195500) do
+ActiveRecord::Schema.define(version: 2019_08_22_210644) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
